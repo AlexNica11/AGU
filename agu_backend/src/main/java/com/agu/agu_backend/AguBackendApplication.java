76c0f9073915101aece74f18ce87279e5e7cba36
AlexNica <alexandru.nica2@student.upt.ro>
2023-10-25 19:53:26 +0300

    initial spring project

---
+++ agu_backend/src/main/java/com/agu/agu_backend/AguBackendApplication.java
@@ -0,0 +1,13 @@
+package com.agu.agu_backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AguBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AguBackendApplication.class, args);
+	}
+
+}
---
+++ agu_backend/src/test/java/com/agu/agu_backend/AguBackendApplicationTests.java
@@ -0,0 +1,15 @@
+package com.agu.agu_backend;
+
+import org.junit.jupiter.api.Disabled;
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@Disabled
+@SpringBootTest
+class AguBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
